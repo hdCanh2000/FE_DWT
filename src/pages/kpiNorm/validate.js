1fb83f57f8f763706a7c59315e27ae4562b5491e
TrinhDat <cv.trinhvietdat1805@gmail.com>
2022-09-14 14:50:54 +0700

    update kpiNorm

--- src/pages/kpiNorm/validate.js
+++ src/pages/kpiNorm/validate.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 const validate = Yup.object({
-	name: Yup.string().required('Vui lòng nhập tên cấp nhân sự'),
+	name: Yup.string().required('Vui lòng nhập tên định mức KPI'),
 	department: Yup.object().required('Vui lòng chọn phòng ban'),
 	// description: Yup.string().required('Vui lòng nhập mô tả'),
 	// unit: Yup.string().required('Vui lòng nhập đơn vị'),
