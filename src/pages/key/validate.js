996848bd534ae5f93238168a9f434a8cd838d1b0
unknown <chutrunghieu12@gmail.com>
2022-09-15 17:25:18 +0700

    fix-key-screen

--- src/pages/key/validate.js
+++ src/pages/key/validate.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 const validate = Yup.object().shape({
-	unit: Yup.number()
+	unit: Yup.string()
 		.required('Vui lòng chọn đơn vị đo lường chỉ số key'),
 	name: Yup.string()
 		.max(30, 'Tên chỉ số key tối đa 30 kí tự')
