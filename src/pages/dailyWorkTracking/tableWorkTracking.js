8ca7c35c8d82a7d616e1ac7f068b5305ed448ccf
TrinhDat <cv.trinhvietdat1805@gmail.com>
2022-09-26 09:24:41 +0700

    update

---
+++ src/pages/dailyWorkTracking/tableWorkTracking.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const TableWorkTrack = () => {
+	return (
+		<div>
+			<table className='table table-modern mb-0'>
+				<thead>
+					<tr>
+						<th>Nhệm vụ</th>
+						<th className='text-center'>Số lượng</th>
+						<th className='text-center'>Đơn vị tính</th>
+						<th className='text-center'>Ghi Chú</th>
+						<th className='text-center'>Thời hạn</th>
+						<th className='text-center'>Kế hoạch dự kiến</th>
+					</tr>
+				</thead>
+				{/* <tbody>
+            </tbody> */}
+			</table>
+		</div>
+	);
+};
+export default TableWorkTrack;
