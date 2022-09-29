a37af40a4d9a112a514578a4cc9776e1d4769841
TrinhDat <cv.trinhvietdat1805@gmail.com>
2022-09-29 09:39:48 +0700

    update Aside

--- src/layout/Aside/Aside.js
+++ src/layout/Aside/Aside.js
@@ -4,14 +4,26 @@ import Navigation from '../Navigation/Navigation';
 import { demoPages } from '../../menu';
 import Card from '../../components/bootstrap/Card';
 import Logo from '../../assets/logos/logo.png';
+// import Button from '../../components/bootstrap/Button';
 
 const Aside = () => {
+	// const [isShow, setIsShow] = React.useState(true);
+	// const handleOpen = () => {
+	// 	setIsShow(!isShow);
+	// };
 	return (
 		<div className='position-fixed aside-head top-0 left-0' style={{ maxWidth: 260 }}>
-			<Link to='/'>
-				<img src={Logo} alt='logo' style={{ width: '80%', marginLeft: '10%' }} />
-			</Link>
 			<Card className='mt-4' style={{ height: '100vh' }}>
+				<div style={{ marginTop: '10px' }}>
+					<Link to='/'>
+						<img src={Logo} alt='logo' style={{ width: '60%', marginLeft: '18%' }} />
+					</Link>
+					{/* <Button
+						icon='LastPage'
+						style={{ border: '1px solid #DCDCDC' }}
+						onClick={handleOpen}
+					/> */}
+				</div>
 				<Navigation menu={demoPages} id='aside-demo-pages' />
 				<div className='p-3'>* Đây chỉ là phiên bản thử nghiệm *</div>
 			</Card>
