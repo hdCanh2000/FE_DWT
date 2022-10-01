3e3330ba266f9014fd3ff81f68fd7d5ddfbb7d40
TrinhDat <cv.trinhvietdat1805@gmail.com>
2022-10-01 12:01:01 +0700

    update employee trong department

--- src/pages/department/UserDetailPage.jsx
+++ src/pages/department/UserDetailPage.jsx
@@ -8,10 +8,44 @@ import TableCommon from '../common/ComponentCommon/TableCommon';
 import { addEmployee, updateEmployee } from '../employee/services';
 import Popovers from '../../components/bootstrap/Popovers';
 import EmployeeForm from '../employee/EmployeeForm';
+import useDarkMode from '../../hooks/useDarkMode';
+import { getAllPosition } from '../position/services';
+import verifyPermissionHOC from '../../HOC/verifyPermissionHOC';
+import { getAllUser } from '../work-management/mission/services';
 
 // eslint-disable-next-line prettier/prettier, react/prop-types
-const DepartmentDetailPage = ({dataUser}) => {
+const DepartmentDetailPage = ({dataUser}) => { 
+	const [user, setUser] = React.useState([]);
+	const { darkModeStatus } = useDarkMode();
 	const { addToast } = useToasts();
+	const [positions, setPositions] = React.useState([]);
+	React.useEffect(() => {
+		const fetch = async () => {
+			const response = await getAllUser();
+			const result = await response.data;
+			// eslint-disable-next-line react/prop-types
+			setUser(result.filter((item) => item.departmentId === dataUser?.id));
+		};
+		fetch();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+	React.useEffect(() => {
+		const fecth = async () => {
+			const response = await getAllPosition();
+			const result = await response.data;
+			setPositions(
+				[...result].map((item) => {
+					return {
+						...item,
+						label: item.name,
+						value: item.id,
+					};
+				}),
+			);
+		};
+		fecth();
+	}, []);
+
 	const columns = [
 		{
 			title: 'Họ và tên',
@@ -30,22 +64,12 @@ const DepartmentDetailPage = ({dataUser}) => {
 			isShow: true,
 		},
 		{
-			title: 'Ngày sinh',
-			id: 'dateOfBirth',
-			key: 'dateOfBirth',
-			type: 'date',
+			title: 'SĐT',
+			id: 'phone',
+			key: 'phone',
+			type: 'text',
 			align: 'center',
-			isShow: true,
-			format: (value) => value && `${moment(`${value}`).format('DD-MM-YYYY')}`,
-		},
-		{
-			title: 'Ngày gia nhập',
-			id: 'dateOfJoin',
-			key: 'dateOfJoin',
-			type: 'date',
-			align: 'center',
-			isShow: true,
-			format: (value) => value && `${moment(`${value}`).format('DD-MM-YYYY')}`,
+			isShow: false,
 		},
 		{
 			title: 'Email',
@@ -56,20 +80,23 @@ const DepartmentDetailPage = ({dataUser}) => {
 			isShow: true,
 		},
 		{
-			title: 'Số điện thoại',
-			id: 'phone',
-			key: 'phone',
-			type: 'text',
-			align: 'center',
+			title: 'Vị trí làm việc',
+			id: 'position',
+			key: 'position',
+			type: 'select',
+			align: 'left',
 			isShow: true,
+			render: (item) => <span>{item?.position?.name || ''}</span>,
+			options: positions,
+			isMulti: false,
 		},
 		{
 			title: 'Địa chỉ',
 			id: 'address',
 			key: 'address',
 			type: 'textarea',
-			align: 'left',
-			isShow: true,
+			align: 'center',
+			isShow: false,
 			render: (item) => (
 				<Popovers desc={item?.address} trigger='hover'>
 					<div
@@ -87,10 +114,28 @@ const DepartmentDetailPage = ({dataUser}) => {
 			),
 		},
 		{
-			title: 'Chức vụ',
-			id: 'position',
-			key: 'position',
-			type: 'select',
+			title: 'Ngày sinh',
+			id: 'dateOfBirth',
+			key: 'dateOfBirth',
+			type: 'date',
+			align: 'center',
+			isShow: false,
+			format: (value) => value && `${moment(`${value}`).format('DD-MM-YYYY')}`,
+		},
+		{
+			title: 'Ngày tham gia',
+			id: 'dateOfJoin',
+			key: 'dateOfJoin',
+			type: 'date',
+			align: 'center',
+			isShow: false,
+			format: (value) => value && `${moment(`${value}`).format('DD-MM-YYYY')}`,
+		},
+		{
+			title: 'Vai trò',
+			id: 'role',
+			key: 'role',
+			type: 'singleSelect',
 			align: 'center',
 			isShow: true,
 			format: (value) => (value === 1 ? 'Quản lý' : 'Nhân viên'),
@@ -98,11 +143,13 @@ const DepartmentDetailPage = ({dataUser}) => {
 				{
 					id: 1,
 					text: 'Quản lý',
+					label: 'Quản lý',
 					value: 1,
 				},
 				{
 					id: 2,
 					text: 'Nhân viên',
+					label: 'Nhân viên',
 					value: 0,
 				},
 			],
@@ -116,20 +163,26 @@ const DepartmentDetailPage = ({dataUser}) => {
 			isShow: true,
 			format: (value) => (value === 1 ? 'Đang hoạt động' : 'Không hoạt động'),
 		},
+
 		{
 			title: 'Hành động',
 			id: 'action',
 			key: 'action',
 			align: 'center',
 			render: (item) => (
-				<Button
-					isOutline
-					color='success'
-					isLight
-					className='text-nowrap mx-2'
-					icon='Edit'
-					onClick={() => handleOpenActionForm(item)}
-				/>
+				<>
+					{verifyPermissionHOC(
+						<Button
+							isOutline={!darkModeStatus}
+							color='success'
+							isLight={darkModeStatus}
+							className='text-nowrap mx-1'
+							icon='Edit'
+							onClick={() => handleOpenActionForm(item)}
+						/>,
+						['admin'],
+					)}
+				</>
 			),
 			isShow: false,
 		},
@@ -149,19 +202,29 @@ const DepartmentDetailPage = ({dataUser}) => {
 	const handleSubmitEmployeeForm = async (data) => {
 		const dataSubmit = {
 			id: data?.id,
-			name: data.name,
-			departmentId: data.departmentId,
-			department: data.department,
-			code: data.code,
-			email: data.email,
+			name: data?.name,
+			departmentId: data?.department?.value,
+			department: {
+				id: data?.department?.value,
+				name: data?.department?.label,
+			},
+			code: data?.code,
+			email: data?.email,
 			password: '123456',
-			dateOfBirth: data.dateOfBirth,
-			dateOfJoin: data.dateOfJoin,
-			phone: data.phone,
-			address: data.address,
-			position: Number.parseInt(data.position, 10),
-			status: Number(data.status),
-			roles: Number.parseInt(data.position, 10) === 1 ? ['manager'] : ['user'],
+			dateOfBirth: data?.dateOfBirth,
+			dateOfJoin: data?.dateOfJoin,
+			phone: data?.phone,
+			address: data?.address,
+			positionId: data?.position?.value,
+			position: {
+				id: data?.position?.value,
+				name: data?.position?.label,
+				value: data?.position?.value,
+				label: data?.position?.label,
+			},
+			role: Number.parseInt(data?.role, 10),
+			status: Number(data?.status),
+			roles: Number.parseInt(data?.role, 10) === 1 ? ['manager'] : ['user'],
 		};
 		if (data.id) {
 			try {
@@ -221,7 +284,7 @@ const DepartmentDetailPage = ({dataUser}) => {
 					className='table table-modern mb-0'
 					columns={columns}
 					// eslint-disable-next-line react/prop-types
-					data={dataUser.users}
+					data={user}
 				/>
 			</div>
 			<EmployeeForm
