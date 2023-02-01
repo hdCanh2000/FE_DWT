59bbeb23b133d3dfa11f06bc2205e848dbb6c5e7
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-02-01 10:34:55 +0700

    change file name

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -78,3 +78,11 @@ export const getFirstAndLastDateOfMonth = () => {
 		lastDay,
 	};
 };
+
+export const removeAccents = (str) => {
+	return str
+		.normalize('NFD')
+		.replace(/[\u0300-\u036f]/g, '')
+		.replace(/đ/g, 'd')
+		.replace(/Đ/g, 'D');
+};
