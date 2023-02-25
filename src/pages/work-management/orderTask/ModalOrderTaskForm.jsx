3bdfc6306e62edb00f078301427c7e58d4d66c15
vanminh-1809 <nvmnvm1818@gmail.com>
2023-02-25 10:06:54 +0700

    fix

--- src/pages/work-management/orderTask/ModalOrderTaskForm.jsx
+++ src/pages/work-management/orderTask/ModalOrderTaskForm.jsx
@@ -159,7 +159,7 @@ const ModalOrderTaskForm = ({ open, onClose, data, isCreate, onSuccess, onUpdate
 					</Col>
 					<Col span={12}>
 						<Form.Item
-							name='userId'
+							name='userName'
 							label='Người đảm nhiệm'
 							rules={[{ required: true, message: 'Vui lòng chọn người đảm nhiệm' }]}>
 							<Select
